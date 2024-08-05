24e4cbf91e0287dfddbf01388ec369410d661e69
Hussam <hosamafiky@gmail.com>
2024-08-05 19:59:35 +0300

    refactor: Remove unused import statement for theme_cubit in presentation_imports.template.ts

--- src/medina_clean_template/presentation/presentation_imports.template.ts
+++ src/medina_clean_template/presentation/presentation_imports.template.ts
@@ -12,7 +12,6 @@ import '../../../core/extensions/sheet.dart';
 import '../../../core/extensions/spaced_column.dart';
 import '../../../core/helpers/dependency_helper.dart';
 import '../../../core/navigation/navigator.dart';
-import '../../../core/shared_cubits/theme/theme_cubit.dart';
 import '../../../core/shared_widgets/core_widgets/main_app_bar.dart';
 import '../../../core/standards/usecase_status.dart';
 import '../domain/domain_imports.dart';
