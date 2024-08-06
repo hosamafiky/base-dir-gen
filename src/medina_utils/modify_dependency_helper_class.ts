9f2ae61aa51828ca0562927150a10bd1fefdfb84
DESKTOP-PBJO0CD\hp <hosamafiky@gmail.com>
2024-08-06 12:36:45 +0300

    refactor: Add setup for feature-specific dependencies in modifyDependencyHelperClass.ts

--- src/medina_utils/modify_dependency_helper_class.ts
+++ src/medina_utils/modify_dependency_helper_class.ts
@@ -1,37 +1,33 @@
-import { workspace } from "vscode";
+import { window, workspace } from "vscode";
 import { getFileContent } from "../utils/get_file_content";
 import { getPascalCase } from "../utils/pascal-case";
-
 export async function modifyDependencyHelperClass(featureName: string) {
   const upperCamelCaseFeatureName = getPascalCase(featureName);
 
-  workspace.findFiles("**/dependency_helper.dart").then((files) => {
-    files.forEach((file) => {
-      getFileContent(file.fsPath).then(async (content) => {
-        if (
-          content.includes("void registerDependencies() {") &&
-          !content.includes(`setUp${upperCamelCaseFeatureName}sDependencies();`)
-        ) {
-          content = content.replace(
-            `void registerDependencies() {
-    setUpGeneralDependencies();`,
-            `void registerDependencies() {
-    setUpGeneralDependencies();
+  let files = await workspace.findFiles("**/dependency_helper.dart");
+
+  files.forEach(async (file) => {
+    let content = await getFileContent(file.fsPath);
+    if (
+      content.includes("void registerDependencies() {") &&
+      !content.includes(`setUp${upperCamelCaseFeatureName}sDependencies();`)
+    ) {
+      let mContent = content.replace(
+        `setUpGeneralDependencies();`,
+        `setUpGeneralDependencies();
     setUp${upperCamelCaseFeatureName}Dependencies();`
-          );
+      );
 
-          content = content.replace(
-            `import 'package:get_it/get_it.dart';
-`,
-            `import 'package:get_it/get_it.dart';
-
+      let modContent = mContent.replace(
+        `import '../../features/home/di/setup_home_dependencies.dart';`,
+        `import '../../features/home/di/setup_home_dependencies.dart';
 import '../../features/${featureName}/di/setup_${featureName}_dependencies.dart';`
-          );
+      );
 
-          const contentArray = new TextEncoder().encode(content);
-          await workspace.fs.writeFile(file, contentArray);
-        }
-      });
-    });
+      window.showInformationMessage(modContent);
+
+      const contentArray = new TextEncoder().encode(modContent);
+      await workspace.fs.writeFile(file, contentArray);
+    }
   });
 }
