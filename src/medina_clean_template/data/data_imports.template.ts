df82882383f599545c1e155a004959f06e1d135e
HUSSAM <hosamafiky@gmail.com>
2024-08-10 10:15:06 +0300

    refactor: Import easy_localization package for localization support

--- src/medina_clean_template/data/data_imports.template.ts
+++ src/medina_clean_template/data/data_imports.template.ts
@@ -2,13 +2,14 @@ export function getMedinaDataImportsTemplate(featureName: string) {
   return `import 'dart:convert';
 
 import 'package:dartz/dartz.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 import '../../../../core/helpers/dependency_helper.dart';
-import '../../../../core/networking/api_constants.dart';
 import '../../../../core/networking/api_request.dart';
 import '../../../../core/networking/api_service.dart';
 import '../../../core/error/failures.dart';
 import '../../../core/extensions/error_handler_extension.dart';
+import '../../../core/networking/response_model.dart';
 import '../domain/domain_imports.dart';
 
 part '../data/datasources/${featureName}_remote_data_source.dart';
